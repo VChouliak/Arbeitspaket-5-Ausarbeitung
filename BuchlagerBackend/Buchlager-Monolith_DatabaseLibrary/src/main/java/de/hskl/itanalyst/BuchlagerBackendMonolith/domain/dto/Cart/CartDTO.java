021f27bac326e4ebb6a7783830dbb7e09567abf5
VChouliak <chouliak@hotmail.com>
2022-03-03 10:49:26 +0100

    Delete BuchlagerBackend module

--- BuchlagerBackend/Buchlager-Monolith_DatabaseLibrary/src/main/java/de/hskl/itanalyst/BuchlagerBackendMonolith/domain/dto/Cart/CartDTO.java
+++
@@ -1,9 +0,0 @@
-package de.hskl.itanalyst.BuchlagerBackendMonolith.domain.dto.Cart;
-
-import java.time.LocalDateTime;
-import java.util.Set;
-
-public class CartDTO {
-    public Set<CartItemDTO> items;
-    public LocalDateTime validUntil;
-}
--- BuchlagerBackend/Buchlager-Monolith_DatabaseLibrary/src/main/java/de/hskl/itanalyst/BuchlagerBackendMonolith/domain/dto/Cart/CartItemDTO.java
+++
@@ -1,8 +0,0 @@
-package de.hskl.itanalyst.BuchlagerBackendMonolith.domain.dto.Cart;
-
-import de.hskl.itanalyst.BuchlagerBackendMonolith.domain.dto.Book.BookLightDTO;
-
-public class CartItemDTO {
-    public long amount;
-    public BookLightDTO book;
-}
